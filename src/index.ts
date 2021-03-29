f0cd19ab8037507e22eeb8cac656293888c7651b
Adrien Martin <adrien.v.martin@gmail.com>
2021-03-29 16:13:05 -0600

    Test new sorting method implementation

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,18 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-/* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(sorter.collection);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data); */
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
-linkedList.add(4);
-
-const sorter = new Sorter(linkedList);
-sorter.sort();
-linkedList.print();
+linkedList.add(4); 
+linkedList.sort();
+linkedList.print(); 
