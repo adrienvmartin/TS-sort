4d8dfc49b91a7eef0b4775399f38dcd2af01aab0
Adrien Martin <adrien.v.martin@gmail.com>
2021-03-29 11:25:04 -0600

    Remove old logic and import new Classes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,8 @@
-class Sorter {
-  constructor(public collection: number[] | string) { }
-  
-  sort(): void {
-    const { length } = this.collection;
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
-        // All of this only works if collection is number[]
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        if (typeof this.collection === 'string') {
-          
-        }
-      }
-    }
-  }
-};
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
